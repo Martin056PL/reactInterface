17149a6f059f8203808328638cf17407f70d0fb6
Kamil Wawer <wawerk@tt.com.pl>
2019-07-12 11:14:11 +0200

    Add pagiantion and visual changes in Movie List

--- src/ActorEdit.js
+++ src/ActorEdit.js
@@ -1,106 +1,88 @@
 import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
-import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap';
+import { Button, ButtonGroup, Container, Table } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-class ActorEdit extends Component {
+class ActorList extends Component {
 
-    emptyActor = {
-        actorId: '',
-        firstName: '',
-        lastName: '',
-        age: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      actors: [],
     };
+    this.remove = this.remove.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          actor: this.emptyActor
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+  componentDidMount() {
+    this.setState({isLoading: true});
 
-    async componentDidMount() {
-        if (this.props.match.params.id !== 'new') {
-          const actor = await (await fetch(`http://localhost:8080/rest/actors/${this.props.match.params.id}`)).json();
-          this.setState({actor: actor});
-        }
-    }
-
-    handleChange(event) {
-        const target = event.target;
-        const value = target.value;
-        const actorId = target.id;
-        let actor = {...this.state.actor};
-        actor[actorId] = value;
-        this.setState({actor});
-    }
-
-    async handleSubmit(event) {
-        event.preventDefault();
-        const {actor} = this.state;
-       /*  movies/{movieId}/actors/{actorId} */
-        if(actor.actorId) {
-            await fetch(`http://localhost:8080/rest/movies/${this.props.match.params.movieId}/actors/${actor.actorId}`, {
-                
-                method: 'PUT',
-                headers: {
-                  'Accept': 'application/json',
-                  'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(actor),
-            });
-        } else {
-            await fetch(`http://localhost:8080/rest/movies/${this.props.match.params.movieId}/actors`, {
-                method: 'POST',
-                headers: {
-                  'Accept': 'application/json',
-                  'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(actor),
-            });
-        }
-        
-        this.props.history.push("/movies/" + this.props.match.params.movieId + "/actors");
+    fetch(`http://localhost:8080/rest/movies/${this.props.match.params.id}/actors`)
+      .then(response => response.json())
+      .then(data => this.setState({actors: data, isLoading: false}));
+  }
+/*   movies/{movieId}/actors/{actorId} */
+  async remove(id) {
+    await fetch(`http://localhost:8080/rest/movies/${this.props.match.params.id}/actors/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
       }
+    }).then(() => {
+      let updatedActors = [...this.state.actors].filter(i => i.actorId !== id);
+      this.setState({actors: updatedActors});
+    });
+  }
 
-    render() {
-        const {actor} = this.state;
-        const title = <h2>{actor.actorId ? 'Edit actor' : 'Add actor'}</h2>;
+  render() {
+    const {actors, isLoading} = this.state;
 
-        return  <div>
-                    <Container>
-                        {title}
-                        <Form onSubmit={this.handleSubmit}>
-                        <FormGroup>
-                            <Label for="firstName">First name</Label>
-                            <Input type="text" name="firstName" id="firstName" value={actor.firstName || ''}
-                                onChange={this.handleChange} autoComplete="firstName" required/>
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="lastName">Last name</Label>
-                            <Input type="text" name="lastName" id="lastName" value={actor.lastName || ''}
-                                onChange={this.handleChange} autoComplete="lastName" required/>
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="age">Age</Label>
-                            <Input type="text" name="age" id="age" value={actor.age || ''}
-                                onChange={this.handleChange} autoComplete="age" required/>
-                        </FormGroup>
-                        
-                        <FormGroup>
-                            <Button color="primary" type="submit">Save</Button>{' '}
-                            <Button color="secondary" tag={Link} to={"/movies/" + this.props.match.params.movieId + "/actors"}>Cancel</Button>
-                        </FormGroup>
-                        </Form>
-                    </Container>
-                </div>
+    if (isLoading) {
+      return <p>Loading...</p>;
     }
+
+    const actorList = actors.map(actor => {
+        return( 
+        <tr key={actor.actorId}>
+          <td style={{whiteSpace: 'nowrap'}}>{actor.firstName}</td>
+          <td>{actor.lastName}</td>
+          <td>{actor.age}</td>
+          <td>
+            <ButtonGroup>
+              <Button size="sm" color="primary" tag={Link} to={ "/movies/" + this.props.match.params.id + "/actors/" + actor.actorId}>Edit</Button>
+              <Button size="sm" color="danger" onClick={() => this.remove(actor.actorId)}>Delete</Button>
+            </ButtonGroup>
+          </td>
+        </tr>
+      )});
+
+    return (
+      <div>
+        <Container fluid>
+          <div className="float-right">
+            <Button color="success" tag={Link} to={"/movies/" + this.props.match.params.id + "/actors/new"}>Add actor</Button>
+          </div>
+          <h3>My actors:</h3>
+          <Table className="mt-4">
+            <thead>
+            <tr>
+              <th width="20%">First Name</th>
+              <th width="20%">Last Name</th>
+              <th width="20%">Age</th>
+              <th width="10%">Actor Options</th>
+            </tr>
+            </thead>
+            <tbody>
+            {actorList}
+            </tbody>
+          </Table>
+          <div className="float-left">
+          <Button color="secondary" tag={Link} to={"/movies"}>Back to movie list</Button>
+          </div>
+        </Container>
+      </div>
+    );
+  }
 }
 
-export default withRouter(ActorEdit);
-
-
-/* http://localhost:8080/rest/actors/${actor.actorId} */
-
-/* http://localhost:8080/rest/movies/${this.props.match.params.movieId}/actors/${actor.actorId} */
+export default ActorList;
--- src/MovieList.js
+++ src/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button, ButtonGroup, Container, Table } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import Pagination from 'react-js-pagination';
 
 class MovieList extends Component {
 
@@ -8,17 +9,21 @@ class MovieList extends Component {
     super(props);
     this.state = {
       isLoading: true,
-      movies: []
+      movies: [],
+      activePage:1,
+      itemsCountPerPage:'',
+      totalItemsCount:''
+
     };
     this.remove = this.remove.bind(this);
+    this.fetchURL = this.fetchURL.bind(this)
+    this.handlePageChange = this.handlePageChange.bind(this) 
   }
 
   componentDidMount() {
     this.setState({isLoading: true});
 
-    fetch('http://localhost:8080/rest/movies')
-      .then(response => response.json())
-      .then(data => this.setState({movies: data, isLoading: false}));
+    this.fetchURL(this.state.activePage)
   }
 
   async remove(id) {
@@ -33,6 +38,26 @@ class MovieList extends Component {
       this.setState({movies: updatedmovies});
     });
   }
+
+  handlePageChange(pageNumber) {
+		console.log(`active page is ${pageNumber}`);
+		this.setState({activePage: pageNumber});
+		this.fetchURL(pageNumber, this.state.searchingName);
+	}
+
+  fetchURL(page) {
+		this.setState({isLoading: true})
+			fetch(`http://localhost:8080/rest/movies?page=${page}&size=10`)
+				.then(response => response.json())
+				.then(data => this.setState({
+					movies: data.content,
+
+					totalPages: data.totalPages,
+					itemsCountPerPage: data.size,
+					totalItemsCount: data.totalElements
+				}))
+		this.setState({isLoading: false})
+	}
 
   render() {
     const {movies, isLoading} = this.state;
@@ -53,10 +78,9 @@ class MovieList extends Component {
           </ButtonGroup>
           </td>
           <td>
-            <ButtonGroup>
               <Button size="sm" color="primary" tag={Link} to={"/movies/" + movie.movieId}>Edit</Button>
+              &nbsp;
               <Button size="sm" color="danger" onClick={() => this.remove(movie.movieId)}>Delete</Button>
-            </ButtonGroup>
           </td> 
         </tr>  
       )});
@@ -65,26 +89,40 @@ class MovieList extends Component {
       <div>
         <Container fluid>
           <div className="float-right">
-            <Button color="success" tag={Link} to="/movies/new">new movie</Button>
+            <Button color="secondary" tag={Link} to={"/"} >Back to main menu</Button>
+          &nbsp;&nbsp;&nbsp;
+            <Button color="success" tag={Link} to="/movies/new">New movie</Button>
           </div>
           <h3>My movies:</h3>
           <Table className="mt-4">
             <thead>
             <tr>
-              <th width="20%">Title</th>
-              <th width="20%">Type</th>
-              <th width="20%">Date Premiere</th>
-              <th width="10%">Actors</th>
+              <th width="30%">Title</th>
+              <th width="10%">Type</th>
+              <th width="10%">Date Premiere</th>
+              <th width="7%">Actors</th>
               <th width="10%">Movie Options</th>
             </tr>
             </thead>
             <tbody>
             {movieList}
             </tbody>
-          </Table> 
+          </Table>
           <div className="float-left">
-          <Button color="secondary" tag={Link} to={"/"}>Back to main menu</Button>
-          </div>
+          <Pagination
+							activePage={this.state.activePage}
+							itemsCountPerPage={this.state.itemsCountPerPage}
+							totalItemsCount={this.state.totalItemsCount}
+							pageRangeDisplayed={5}
+							itemClass='page-item'
+							linkClass='btn btn-light'
+							onChange={this.handlePageChange}
+							firstPageText="First page"
+							lastPageText="Last page"
+							prevPageText="Previous page"
+							nextPageText="Next page"
+						/>
+            </div>
         </Container>
       </div>
     );
